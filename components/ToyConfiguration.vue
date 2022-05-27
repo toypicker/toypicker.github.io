3080f595f0e3eae92203a8e1ccd302df134bf8a9
Mikkel Nygaard <mikkel@bnyg.dk>
2022-05-27 08:57:12 +0200

    Basic random pick implemented.

---
+++ components/ToyConfiguration.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-card>
+    <v-card-title>Toy configuration</v-card-title>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,119 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
       <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
+        <v-card-title class="headline"> Play session </v-card-title>
+        <v-row>
+          <v-col v-for="(play, index) in session" :key="index" cols="2">
+            <v-card elevation="2"
+              >{{ play.toy }} on {{ play.player }}
+              <v-btn v-if="playableToys.length > 0" @click="reroll(index)"
+                >Reroll</v-btn
+              >
+            </v-card></v-col
           >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
+          <v-col cols="2" align="center">
+            <v-card elevation="2"
+              ><v-btn v-if="playableToys.length > 0" @click="roll()"
+                ><v-avatar><v-icon>mdi-plus-thick</v-icon></v-avatar></v-btn
+              >
+              <span v-else>No more toys to play with</span>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-card-actions
+          ><v-btn @click="newSession()">New session</v-btn></v-card-actions
+        >
       </v-card>
     </v-col>
   </v-row>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      toys: [
+        {
+          id: 0,
+          name: 'Toy 1',
+          active: true,
+          eligblePlayers: [0, 1],
+        },
+        {
+          id: 1,
+          name: 'Toy 2',
+          active: true,
+          eligblePlayers: [0],
+        },
+        {
+          id: 2,
+          name: 'Toy 3',
+          active: true,
+          eligblePlayers: [1],
+        },
+        {
+          id: 3,
+          name: 'Toy 4',
+          active: false,
+          eligblePlayers: [0, 1],
+        },
+      ],
+      players: [
+        {
+          id: 0,
+          name: 'Mikkel',
+        },
+        {
+          id: 1,
+          name: 'Linda',
+        },
+      ],
+      session: [] as { toyId: number; toy: string; player: string }[],
+    }
+  },
+
+  computed: {
+    playableToys(): { id: number; name: string; players: string[] }[] {
+      return this.toys
+        .filter(
+          (toy) =>
+            toy.active && !this.session.some((play) => play.toyId === toy.id)
+        )
+        .map((toy) => ({
+          id: toy.id,
+          name: toy.name,
+          players: toy.eligblePlayers.map(
+            (playerId) =>
+              this.players.find((player) => player.id === playerId)?.name ??
+              'Unknown'
+          ),
+        }))
+    },
+  },
+  methods: {
+    createRandomPlay(): { toyId: number; toy: string; player: string } {
+      if (!this.playableToys.length) {
+        return { toyId: -1, toy: 'N/A', player: 'N/A' }
+      }
+
+      const randomToy =
+        this.playableToys[Math.floor(Math.random() * this.playableToys.length)]
+      const randomPlayer =
+        randomToy.players[Math.floor(Math.random() * randomToy.players.length)]
+
+      return { toyId: randomToy.id, toy: randomToy.name, player: randomPlayer }
+    },
+    roll() {
+      this.session.push(this.createRandomPlay())
+    },
+    reroll(index: number) {
+      this.session.splice(index, 1, this.createRandomPlay())
+    },
+    newSession() {
+      this.session = []
+    },
+  },
+})
+</script>
